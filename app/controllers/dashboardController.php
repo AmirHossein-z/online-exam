9c0c40c2e5374fbfa97af01a27bbb67ff17aed25
Amirhossein Zareian <amirhossein.zareian.engineer@gmail.com>
2022-12-22 16:16:53 +0330

    dashboard were created

---
+++ app/controllers/dashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+class dashboardController extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        if (!isset($_SESSION['name'])) {
+            header('Location: ' . URL . 'auth/login');
+        }
+    }
+
+    public function index()
+    {
+        $this->header('header');
+        $this->view('dashboard/masterDashboard');
+    }
+}
