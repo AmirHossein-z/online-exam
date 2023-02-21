03e20f8501a3ae21f601b1f986e0c298ba16f8c0
Reza <hello.dehghani@gmail.com>
2023-02-21 23:08:25 +0330

    merge navbar

--- views/layout/navbar.php
+++ views/layout/navbar.php
@@ -138,12 +138,8 @@
       </div>
     </nav>
     <div class="relative md:ml-64 bg-blueGray-50">
-      <nav
-        class="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
-        <div class="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-          <a class="text-white text-sm hidden lg:inline-block font-semibold text-red-500"
-            href="./index.html">داشبورد</a>
 
+<br>
             <!-- 
 
         <ul class="md:hidden items-center flex flex-wrap list-none">
